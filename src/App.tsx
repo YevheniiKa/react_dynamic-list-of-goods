44270db3bd7c01b61273ffa330eda1148053db99
Yevhenii Kaliaiev <eugenyr34@gmail.com>
2025-08-22 23:31:43 +0300

    added stable version, without errors and rendering optimization

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,67 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.scss';
 import { GoodsList } from './GoodsList';
 
-// import { getAll, get5First, getRed } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
+import { getAll, get5First, getRedGoods } from './api/goods';
+import { Good } from './types/Good';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <h1>Dynamic list of Goods</h1>
+export const App: React.FC = () => {
+  const [isSelected, setIsSelected] = useState<Good[]>([]);
+  const [selected, setSelected] = useState<string>('');
 
-    <button type="button" data-cy="all-button">
-      Load all goods
-    </button>
+  useEffect(() => {
+    switch (selected) {
+      case 'allGoods': {
+        getAll().then(setIsSelected);
+        break;
+      }
 
-    <button type="button" data-cy="first-five-button">
-      Load 5 first goods
-    </button>
+      case '5first': {
+        get5First().then(setIsSelected);
+        break;
+      }
 
-    <button type="button" data-cy="red-button">
-      Load red goods
-    </button>
+      case 'allRed': {
+        getRedGoods().then(setIsSelected);
+        break;
+      }
+    }
+  }, [selected]);
 
-    <GoodsList goods={[]} />
-  </div>
-);
+  return (
+    <>
+      <div className="App">
+        <h1>Dynamic list of Goods</h1>
+
+        <button
+          type="button"
+          data-cy="all-button"
+          onClick={() => setSelected('allGoods')}
+        >
+          Load all goods
+        </button>
+
+        <button
+          type="button"
+          data-cy="first-five-button"
+          onClick={() => setSelected('5first')}
+        >
+          Load 5 first goods
+        </button>
+
+        <button
+          type="button"
+          data-cy="red-button"
+          onClick={() => {
+            setSelected('allRed');
+          }}
+        >
+          Load red goods
+        </button>
+
+        <GoodsList goods={isSelected} name={selected} />
+      </div>
+      ;
+    </>
+  );
+};
--- src/GoodsList.tsx
+++ src/GoodsList.tsx
@@ -3,12 +3,13 @@ import { Good } from './types/Good';
 
 type Props = {
   goods: Good[];
+  name: string;
 };
 
-export const GoodsList: React.FC<Props> = ({ goods }) => (
-  <ul>
+export const GoodsList: React.FC<Props> = ({ goods, name }) => (
+  <ul style={name === 'allRed' ? { color: 'red' } : undefined}>
     {goods.map(good => (
-      <li key={good.id} data-cy="good">
+      <li key={good.id} data-cy="good" style={{ color: good.color }}>
         {good.name}
       </li>
     ))}
